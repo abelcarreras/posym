Added Improper rotation Fixed library to handle Td Added function to generate all group operations from showcase ones other fixes

modes_example.py
@@ -1,6 +1,9 @@
 from pyqchem import get_output_from_qchem, Structure, QchemInput
 from pyqchem.parsers.parser_frequencies import basic_frequencies
+from pyqchem.parsers.parser_optimization import basic_optimization
+
 from posym import PointGroup, SymmetryModes
+from pyqchem.tools import get_geometry_from_pubchem
 import numpy as np
 
 
@@ -17,26 +20,50 @@ molecule_water = Structure(coordinates=water,
 
 dicloro = [[ 2.1437,  0.1015, -0.0002],
            [-2.1439, -0.1011, -0.0002],
-           [ 0.5135 , -0.4232, 0.0002],
-           [-0.5132,  0.4227, 0.0002],
-           [ 0.4242, -1.5014, 0.0001],
-           [-0.4237,  1.5009, 0.0001]]
+           [ 0.5135, -0.4232,  0.0002],
+           [-0.5132,  0.4227,  0.0002],
+           [ 0.4242, -1.5014,  0.0001],
+           [-0.4237,  1.5009,  0.0001]]
 dcl_symbols = ['Cl', 'Cl', 'C', 'C', 'H', 'H']
+
+from scipy.spatial.transform import Rotation as R
+
+angles = [100, 200, 300]
+rotmol = R.from_euler('zyx', angles, degrees=True)
+dicloro = rotmol.apply(dicloro)
 
 molecule_ch2cl2 = Structure(coordinates=dicloro,
                             symbols=dcl_symbols,
                             charge=0,
                             multiplicity=1)
 
-for molecule, group in zip([molecule_water, molecule_ch2cl2], ['c2v', 'c2h']):
+molecule_tetracene = get_geometry_from_pubchem('naphthalene')
+methane = get_geometry_from_pubchem('methane')
+print(methane)
+
+for molecule, group in zip([molecule_water, molecule_ch2cl2, methane], ['c2v', 'c2h', 'Td']):
+
     qc_input = QchemInput(molecule,
-                          jobtype='freq',
+                          jobtype='opt',
                           exchange='hf',
-                          basis='6-31G',
-                          # sym_ignore=True,
+                          basis='6-31g',
+                          geom_opt_tol_gradient=1,
+                          geom_opt_tol_energy=1,
+                          geom_opt_max_cycles=500,
                           )
 
-    parsed_data, ee = get_output_from_qchem(qc_input, parser=basic_frequencies, read_fchk=True)
+    parsed_data = get_output_from_qchem(qc_input, parser=basic_optimization, processors=6)
+
+    qc_input = QchemInput(parsed_data['optimized_molecule'],
+                          jobtype='freq',
+                          exchange='hf',
+                          basis='6-31g',
+                          #sym_ignore=True,
+                          )
+
+    print(molecule)
+    parsed_data, ee = get_output_from_qchem(qc_input, parser=basic_frequencies,
+                                            read_fchk=True, processors=6)
 
     molecule_coor = np.array(ee['structure'].get_coordinates())
     molecule_symbols = np.array(ee['structure'].get_symbols())
@@ -47,10 +74,15 @@ for molecule, group in zip([molecule_water, molecule_ch2cl2], ['c2v', 'c2h']):
 
     modes = [np.array(m['displacement']) for m in parsed_data['modes']]
     freqs = [m['frequency'] for m in parsed_data['modes']]
-    print('freqs: ', freqs)
+    # print('freqs: ', freqs)
+    # print(molecule_coor)
 
-    sm = SymmetryModes(group=group, coordinates=molecule_coor, modes=modes, symbols=molecule_symbols, optimize=False)
+    sm = SymmetryModes(group=group, coordinates=molecule_coor, modes=modes, symbols=molecule_symbols, optimize=True)
 
     for i in range(len(modes)):
-        print('m {}:'.format(i + 1), sm.get_state_mode(i))
+        print('m {:2}: {:8.3f} :'.format(i + 1, freqs[i]), sm.get_state_mode(i))
     print('Total: ', sm)
+
+    #print(sm.opt_coordinates)
+    # exit()
+

posym/__init__.py
@@ -117,17 +117,21 @@ class SymmetryModes(SymmetryBase):
         self._mode_measures = []
 
         if optimize:
-            angles = self.get_orientation(ir_table.operations)
+            self._angles = self.get_orientation(ir_table.operations)
         else:
-            angles = [0, 0, 0]
+            self._angles = [0, 0, 0]
 
-        rotmol = R.from_euler('zyx', angles, degrees=True)
-        #rotmol = R.from_rotvec(angles)
+        rotmol = R.from_euler('zyx', self._angles, degrees=True)
 
         for operation in ir_table.operations:
-            mode_m, coor_m = operation.get_measure(self._coordinates, self._modes, self._symbols, orientation=rotmol)
-            self._mode_measures.append(mode_m)
-            self._coor_measures.append(coor_m)
+            operations_dic = ir_table.get_all_operations()
+            mode_measures = []
+            for op in operations_dic[operation.label]:
+                mode_m = op.get_measure(self._coordinates, self._modes, self._symbols, orientation=rotmol)
+                mode_measures.append(mode_m)
+
+            mode_measures = np.average(mode_measures, axis=0)
+            self._mode_measures.append(mode_measures)
 
         total_state = pd.Series(np.add.reduce(self._mode_measures, axis=1), index=ir_table.index)
 
@@ -145,7 +149,7 @@ class SymmetryModes(SymmetryBase):
 
             coor_measures = []
             for operation in operations:
-                mode_m, coor_m = operation.get_measure(self._coordinates, self._modes, self._symbols, orientation=rotmol)
+                coor_m = operation.get_measure_pos(self._coordinates, self._symbols, orientation=rotmol)
                 coor_measures.append(coor_m)
 
             # definition group measure
@@ -154,20 +158,172 @@ class SymmetryModes(SymmetryBase):
         # preliminar scan
         list_m = []
         list_a = []
-        for i in np.arange(0, 360, 36):
-            for j in np.arange(0, 360, 36):
+        for i in np.arange(0, 180, 36):
+            for j in np.arange(0, 180, 36):
                 for k in np.arange(0, 180, 36):
                     list_m.append(optimization_function([i, j, k]))
                     list_a.append([i, j, k])
 
         initial = np.array(list_a[np.nanargmin(list_m)])
         res = minimize(optimization_function, initial, method='CG',
-                       #bounds=((0, 360), (0, 360), (0, 360)),
-                       tol=1e-20)
+                       # bounds=((0, 360), (0, 360), (0, 360)),
+                       # tol=1e-20
+                       )
 
-        print('res', res.x)
         return res.x
 
     @property
-    def get_measure_coor(self):
-        return np.add.reduce(self._coor_measures)
+    def get_measure_pos(self):
+        return np.product(self._coor_measures)
+
+    @property
+    def opt_coordinates(self):
+        rotmol = R.from_euler('zyx', self._angles, degrees=True)
+        return rotmol.apply(self._coordinates)
+
+if __name__ == '__main__':
+
+    from pyqchem import get_output_from_qchem, Structure
+    from pyqchem.tools import get_geometry_from_pubchem
+
+    coordinates = [[ 0.000000000+00,  0.000000000+00,  2.40297090e-01],
+                   [-1.43261539e+00, -1.75444785e-16, -9.61188362e-01],
+                   [ 1.43261539e+00,  1.75444785e-16, -9.61188362e-01]]
+
+    symbols = ['O', 'H', 'H']
+
+    coordinates = [[0, 0, 0],
+                   [ np.sqrt(8/9), 0, -1/3],
+                   [-np.sqrt(2/9), np.sqrt(2/3), -1/3],
+                   [-np.sqrt(2/9), -np.sqrt(2/3), -1/3],
+                   [0, 0, 1]]
+
+    #coordinates = [[ 0.0000000000,    0.0000000000,    0.0000000000],
+    #               [ 0.5541000000,    0.7996000000,    0.4965000000],
+    #               [ 0.6833000000,   -0.8134000000,   -0.2536000000],
+    #               [-0.7782000000,   -0.3735000000,    0.6692000000],
+    #               [-0.4593000000,    0.3874000000,   -0.9121000000]]
+
+    coordinates = [[-3.11301739e-06,  1.12541091e-05, -7.97835696e-06],
+                   [-6.58614327e-02, -7.77865103e-01, -7.63860353e-01],
+                   [-3.64119514e-02,  9.82136003e-01, -4.76386979e-01],
+                   [-8.37587195e-01, -9.93456888e-02,  6.93899368e-01],
+                   [ 9.39879258e-01, -1.04992740e-01,  5.46395830e-01]]
+
+    symbols = ['C', 'H', 'H', 'H', 'H']
+
+
+    if False:
+        mol = get_geometry_from_pubchem('methane')
+        coordinates = mol.get_coordinates()
+        symbols = mol.get_symbols()
+
+    from posym.operations.rotation import Rotation, rotation
+    from posym.operations.reflection import Reflection, reflection
+    from posym.operations.irotation import ImproperRotation
+    from posym.operations import get_permutation_simple, get_cross_distance_table
+
+    import matplotlib.pyplot as plt
+
+    if True:
+        def optimization_function(angles):
+            rotmol = R.from_euler('zyx', angles, degrees=True)
+            operation = Rotation(label='C3', axis=[0, 0, 1], order=3)
+            # print(operation._axis, operation._order)
+            coor_m = operation.get_measure_pos(np.array(coordinates), symbols, orientation=rotmol)
+            operation = Reflection(label='s', axis=[0, 1, 0])
+            # print(operation._axis, operation._order)
+            coor_m2 = operation.get_measure_pos(np.array(coordinates), symbols, orientation=rotmol)
+            operation = Rotation(label='C2', axis=[np.sqrt(2/9), 0, 1/3], order=2)
+            coor_m3 = operation.get_measure_pos(np.array(coordinates), symbols, orientation=rotmol)
+            operation = ImproperRotation(label='S4', axis=[np.sqrt(2/9), 0, 1/3], order=4)
+            coor_m4 = operation.get_measure_pos(np.array(coordinates), symbols, orientation=rotmol)
+
+            #print(coor_m + coor_m2 + coor_m3 + coor_m4)
+            return coor_m + coor_m2 + coor_m3 + coor_m4
+            #return np.product([coor_m, coor_m2, coor_m3, coor_m4])
+
+
+        #exit()
+
+        list_m = []
+        list_a = []
+        for i in np.arange(0, 180, 36):
+            for j in np.arange(0, 180, 36):
+                for k in np.arange(0, 180, 36):
+                    list_m.append(optimization_function([i, j, k]))
+                    list_a.append([i, j, k])
+
+        initial = np.array(list_a[np.nanargmin(list_m)])
+        #initial = [143.63674808,  -4.31338625,  70.70307201]
+        # initial = [43.22189286,  43.86797247, 103.83908054]
+        res = minimize(optimization_function, initial, method='CG',
+                       #tol=1e20
+                       )
+
+        print('res', res.x)
+        print('test', optimization_function(res.x))
+
+        # res.x = [43.22189286,  43.86797247, 103.83908054]
+
+        rotmol = R.from_euler('zyx', res.x, degrees=True)
+
+    else:
+        angles = [43.22189286,  43.86797247, 103.83908054]
+        rotmol = R.from_euler('zyx', angles, degrees=True)
+
+
+        #print(optimization_function(res.x))
+        #rotmol = R.from_euler('zyx', res.x, degrees=True)
+
+
+    #operation = Rotation(label='C3', axis=[0, 0, 1], order=3)
+    #rotmol = R.from_euler('zyx', [0, 0, 0], degrees=True)
+    #mode_m, coor_m = operation.get_measure(np.array(coordinates), modes, symbols, orientation=rotmol)
+    #print('measure', coor_m)
+    #rotmol = R.from_euler('zyx', [360./3, 0, 0], degrees=True)
+    #print('---')
+
+    print(Structure(coordinates, symbols))
+
+    # rotations
+    #rotated_axis = rotmol.apply([0, 0, 1])
+    #operation = rotation(2*np.pi/3, rotated_axis)
+    #print('axis', rotated_axis)
+
+    # reflection
+    #rotated_axis_r = rotmol.apply([0, 1,  0])
+    #operation = reflection(rotated_axis_r)
+    #print('axis_r', rotated_axis_r)
+
+    # C2 rotation
+    # rotated_axis_i = rotmol.apply([(np.sqrt(8 / 9))/2, 0, (1-1 / 3)/2])
+
+    rotated_axis_i = rotmol.apply([np.sqrt(2 / 9), 0, 1 / 3])
+    operation1 = rotation(2*np.pi/4, rotated_axis_i)
+    operation2 = reflection(rotated_axis_i)
+    operation = np.dot(operation2, operation1)
+    print('axis_i', rotated_axis_i)
+
+    print('det:', np.linalg.det(operation))
+    permu_coor = np.dot(operation, np.array(coordinates).T).T
+
+    print(Structure(permu_coor, symbols))
+
+    #permu_coor = rotmol.apply(coordinates)
+    #print(Structure(permu_coor, symbols))
+
+    distance_table = get_cross_distance_table(coordinates, permu_coor)
+    perm = get_permutation_simple(distance_table)
+    #print(np.round(distance_table[perm], 3))
+    print(perm)
+
+
+    permu_coor = permu_coor[perm]
+
+    print(Structure(permu_coor, symbols))
+
+    print('****')
+    print(permu_coor)
+    a = np.average(np.linalg.norm(np.subtract(coordinates, permu_coor), axis=0))
+    print(a)

posym/ir_tables.py
@@ -5,6 +5,7 @@ from posym.operations.identity import Identity
 from posym.operations.rotation import Rotation
 from posym.operations.reflection import Reflection
 from posym.operations.inversion import Inversion
+from posym.operations.irotation import ImproperRotation
 
 
 def real_radical(m, n):
@@ -73,6 +74,41 @@ class CharTable(pd.DataFrame):
             raise Exception('Multiplicities do not match')
 
         self.attrs['operations'] = operations
+
+    def get_all_operations(self):
+
+        if 'all_operations' in self.attrs:
+            return self.attrs['all_operations']
+
+        total_op = list(self.operations)
+        for op1 in self.operations:
+            for op2 in self.operations:
+                opt_list = op1 * op2
+                for op in opt_list:
+                    if op not in total_op:
+                        total_op.append(op)
+
+        repeat = True
+        while repeat is True:
+            repeat = False
+            for op1 in tuple(total_op):
+                for op2 in tuple(total_op):
+                    opt_list = op1 * op2
+                    for op in opt_list:
+                        if op not in total_op:
+                            total_op.append(op)
+                            repeat = True
+
+        operation_dict = {}
+        for op in total_op:
+            if not op.label in operation_dict:
+                operation_dict[op.label] = [op]
+            else:
+                operation_dict[op.label].append(op)
+
+        self.attrs['all_operations'] = operation_dict
+
+        return self.attrs['all_operations']
 
     @property
     def rotations(self):
@@ -153,22 +189,25 @@ ir_table_list = [
                  translations=['Ag', 'Ag', 'Ag'],  # Rx, Ry, Rz
                  multiplicities=[1, 1]),
 
-    OldCharTable({'A': pd.Series([+1, +1], index=['E', 'C3']),
-                  'E': pd.Series([+2, -1], index=['E', 'C3']),
-                  },
-                 name='C3',
-                 rotations=['E', 'E', 'A'],  # x, y, z
-                 translations=['E', 'E', 'A'],  # Rx, Ry, Rz
-                 multiplicities=[1, 2]),
+    CharTable('C3',
+              [Identity(label='E'), Rotation(label='C3', axis=[0, 0, 1], order=3)],
+              {'A': pd.Series([+1, +1]),
+               'E': pd.Series([+2, -1])
+               },
+              rotations=['E', 'E', 'A'],  # x, y, z
+              translations=['E', 'E', 'A'],  # Rx, Ry, Rz
+              multiplicities=[1, 2]),
 
-    OldCharTable({'A' : pd.Series([+1, +1, +1], index=['E', 'C5', 'C5_2']),
-                 'E1': pd.Series([+2, real_radical(2, 5), real_radical(4, 5)], index=['E', 'C5', 'C5_2']),
-                 'E2': pd.Series([+2, real_radical(4, 5), real_radical(2, 5)], index=['E', 'C5', 'C5_2']),
-                  },
-                 name='C5',
-                 rotations=['E1', 'E1', 'A'],  # x, y, z
-                 translations=['E1', 'E1', 'A'],  # Rx, Ry, Rz
-                 multiplicities=[1, 2, 2]),
+    CharTable('C5',
+              [Identity(label='E'), Rotation(label='C5', axis=[0, 0, 1], order=5),
+               Rotation(label='C5_2', axis=[0, 0, 1], order=5)],
+              {'A' : pd.Series([+1, +1, +1]),
+               'E1': pd.Series([+2, real_radical(2, 5), real_radical(4, 5)]),
+               'E2': pd.Series([+2, real_radical(4, 5), real_radical(2, 5)])
+               },
+              rotations=['E1', 'E1', 'A'],  # x, y, z
+              translations=['E1', 'E1', 'A'],  # Rx, Ry, Rz
+              multiplicities=[1, 2, 2]),
 
     CharTable('C2h',
               [Identity(label='E'), Rotation(label='C2', axis=[0, 0, 1], order=2),
@@ -192,31 +231,36 @@ ir_table_list = [
               rotations=['B2', 'B1', 'A2'],
               translations=['B1', 'B2', 'A1'],
               multiplicities=[1, 1, 1, 1]),
-    OldCharTable({'A1': pd.Series([+1, +1, +1], index=['E', 'C3', 'sv']),
-                  'A2': pd.Series([+1, +1, -1], index=['E', 'C3', 'sv']),
-                  'E' : pd.Series([+2, -1,  0], index=['E', 'C3', 'sv']),
-                  },
-                 name='C3v',
-                 rotations=['E', 'E', 'A2'],  # x, y, z
-                 translations=['E', 'E', 'A1'],  # Rx, Ry, Rz
-                 multiplicities=[1, 2, 3]),
-
-    OldCharTable({'A1': pd.Series([+1, +1, +1, +1, +1], index=['E', 'C3', 'C2', 'S4', 'sd']),
-                  'A2': pd.Series([+1, +1, +1, -1, -1], index=['E', 'C3', 'C2', 'S4', 'sd']),
-                  'E' : pd.Series([+2, -1, +2,  0,  0], index=['E', 'C3', 'C2', 'S4', 'sd']),
-                  'T1': pd.Series([+3,  0, -1, +1, -1], index=['E', 'C3', 'C2', 'S4', 'sd']),
-                  'T2': pd.Series([+3,  0, -1, -1, +1], index=['E', 'C3', 'C2', 'S4', 'sd']),
-                  },
-                 name='Td',
-                 rotations=['T1', 'T1', 'T1'],
-                 translations=['T2', 'T2', 'T2'],
-                 multiplicities=[1, 8, 3, 6, 6]),
+    CharTable('C3v',
+              [Identity(label='E'), Rotation(label='C3', axis=[0, 0, 1], order=3),
+               Reflection(label='sv', axis=[1, 0, 0])],
+              {'A1': pd.Series([+1, +1, +1]),
+               'A2': pd.Series([+1, +1, -1]),
+               'E':  pd.Series([+2, -1,  0])
+               },
+              rotations=['E', 'E', 'A2'],
+              translations=['E', 'E', 'A1'],
+              multiplicities=[1, 2, 3]),
+    CharTable('Td',
+              [Identity(label='E'), Rotation(label='C3', axis=[0, 0, 1], order=3),
+               Rotation(label='C2', axis=[np.sqrt(2/9), 0, 1/3], order=2),
+               ImproperRotation(label='S4', axis=[np.sqrt(2/9), 0, 1/3], order=4),
+               Reflection(label='sd', axis=[0, 1, 0])],
+              {'A1': pd.Series([+1, +1, +1, +1, +1]),
+               'A2': pd.Series([+1, +1, +1, -1, -1]),
+               'E' : pd.Series([+2, -1, +2,  0,  0]),
+               'T1': pd.Series([+3,  0, -1, +1, -1]),
+               'T2': pd.Series([+3,  0, -1, -1, +1])
+               },
+              rotations=['T1', 'T1', 'T1'],
+              translations=['T2', 'T2', 'T2'],
+              multiplicities=[1, 8, 3, 6, 6]),
 ]
 
 if __name__ == '__main__':
 
     print(ir_table_list[-1].sort_index().index)
-    exit()
+    # exit()
 
     a = np.array([ v for v in ir_table_list[-1].T.values]).T
     print('trans matrix')
@@ -244,3 +288,16 @@ if __name__ == '__main__':
     print('et1')
     print(np.dot(a_i, et1))
 
+    print('--------')
+
+    td = ir_table_list[-1]
+
+    print(0, 0, 0)
+    print(0, 0, 1)
+    print(np.sqrt(8 / 9), 0, -1 / 3)
+    print(0, 1, 0)
+    exit()
+
+    #[Identity(label='E'), Rotation(label='C3', axis=[0, 0, 1], order=3),
+    # Rotation(label='C2', axis=[np.sqrt(8 / 9), 0, -1 / 3], order=2),
+    # ImproperRotation(label='S4', axis=[np.sqrt(8 / 9), 0, -1 / 3], order=4),

posym/operations/__init__.py
@@ -1,20 +1,21 @@
 import numpy as np
 
 
-def get_distance_table(coordinates, coordinates_2):
+def get_cross_distance_table(coordinates_1, coordinates_2):
 
-    coordinates = np.array(coordinates)
-    coordinates2 = np.array(coordinates_2)
+    coordinates_1 = np.array(coordinates_1)
+    coordinates_2 = np.array(coordinates_2)
 
-    distances = np.zeros((len(coordinates), len(coordinates2)))
+    distances = np.zeros((len(coordinates_1), len(coordinates_2)))
 
-    for i, c1 in enumerate(coordinates):
-        for j, c2 in enumerate(coordinates2):
+    for i, c1 in enumerate(coordinates_1):
+        for j, c2 in enumerate(coordinates_2):
+            # print(i, j, c1, c2, np.linalg.norm(c1 - c2))
             distances[i, j] = np.linalg.norm(c1 - c2)
     return distances
 
 
-def get_permutation_simple(distance_table):
+def get_permutation_simple(distance_table, symbols):
 
     def evaluation(i, j, row1, row2):
         from scipy.stats import norm
@@ -34,20 +35,28 @@ def get_permutation_simple(distance_table):
     control = True
     while control:
         control = False
-        for a in range(len(distance_table), 1, -1):
+        for a in range(len(distance_table)-1, 1, -1):
             for i in range(len(distance_table)):
                 j = i+a if i+a < len(distance_table) else i+a - len(distance_table)
 
                 row_1 = distance_table[i]
                 row_2 = distance_table[j]
 
-                if evaluation_fast(i, j, row_1, row_2) > evaluation_fast(j, i, row_1, row_2):
-                    distance_table[[i, j]] = distance_table[[j, i]]
-                    perm[[i, j]] = perm[[j, i]]
-                    control = True
+                if symbols[i] == symbols[j]:
+                    if evaluation_fast(i, j, row_1, row_2) > evaluation_fast(j, i, row_1, row_2):
+                        distance_table[[i, j]] = distance_table[[j, i]]
+                        perm[[i, j]] = perm[[j, i]]
+                        control = True
 
+                # print(i, j, evaluation_fast(i, j, row_1, row_2), evaluation_fast(j, i, row_1, row_2))
+                # print(distance_table)
+
+    def inverse_perm(perm):
+        return [list(perm).index(j) for j in range(len(perm))]
+
+    # print('-->\n', np.round(distance_table, decimals=3))
     # print(np.round(np.diag(distance_table), decimals=3))
-    return tuple(perm)
+    return inverse_perm(perm)
 
 
 class Operation:
@@ -58,8 +67,8 @@ class Operation:
     def get_permutation(self, operation, coordinates, symbols):
         operated_coor = np.dot(operation, coordinates.T).T
 
-        distance_table = get_distance_table(coordinates, operated_coor)
-        perm = get_permutation_simple(distance_table)
+        distance_table = get_cross_distance_table(coordinates, operated_coor)
+        perm = get_permutation_simple(distance_table, symbols)
         permu_coor = operated_coor[list(perm)]
 
         measure = np.average(np.linalg.norm(np.subtract(coordinates, permu_coor), axis=0))

posym/operations/identity.py
@@ -6,9 +6,30 @@ class Identity(Operation):
     def __init__(self, label):
         super().__init__(label)
 
+    def __hash__(self):
+        return hash((self._label))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
     def get_measure(self, coordinates, modes, symbols, orientation=None):
         self._measure_mode = [1.0] * len(modes)
         self._measure_coor = 0.0
 
-        return np.array(self._measure_mode), self._measure_coor
+        return np.array(self._measure_mode)
+
+    def get_measure_pos(self, coordinates, symbols, orientation=None):
+        self._measure_coor = 0.0
+
+        return self._measure_coor
+
+    @property
+    def operation_matrix_list(self):
+        return [np.identity(3)]
+
+    def __mul__(self, other):
+        if not other.__class__.__bases__[0] is Operation:
+            raise Exception('Product only defined between Operation subclasses')
+        else:
+            return [Identity(self._label)]
 

posym/operations/inversion.py
@@ -14,27 +14,44 @@ class Inversion(Operation):
     def __init__(self, label):
         super().__init__(label)
 
+    def __hash__(self):
+        return hash((self._label))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+
+
     def get_measure(self, coordinates, modes, symbols, orientation=None):
 
         operation = inversion()
         operated_coor = np.dot(operation, coordinates.T).T
 
-        measure_mode = []
-        measure_coor = []
-
         mesure_coor, permu = self.get_permutation(operation, coordinates, symbols)
 
+        measure_mode = []
         for mode in modes:
             operated_mode = np.dot(operation, prepare_vector(coordinates, mode).T).T - operated_coor
-            #norm_1 = np.linalg.norm(mode, axis=1)
             norm = np.linalg.norm(mode)
-
-
             permu_mode = np.array(operated_mode)[permu]
-            # norm_2 = np.linalg.norm(permu_mode, axis=1)
 
-            #self._measure_mode.append(np.nanmean(np.divide(np.diag(np.dot(mode, permu_mode.T)), norm_1 * norm_2)))
-            measure_mode.append(np.add.reduce(np.diag(np.dot(mode, permu_mode.T)))/norm)
+            measure_mode.append(np.trace(np.dot(mode, permu_mode.T))/norm)
 
-        return np.array(measure_mode), mesure_coor
+        return np.array(measure_mode)
 
+    def get_measure_pos(self, coordinates, symbols, orientation=None):
+
+        operation = inversion()
+        mesure_coor, permu = self.get_permutation(operation, coordinates, symbols)
+
+        return mesure_coor
+
+    @property
+    def operation_matrix_list(self):
+        return [inversion()]
+
+    def __mul__(self, other):
+        if not other.__class__.__bases__[0] is Operation:
+            raise Exception('Product only defined between Operation subclasses')
+        else:
+            return [Inversion(self._label)]

posym/operations/irotation.py
@@ -1,23 +1,15 @@
 from posym.operations import Operation
-from scipy.spatial.transform import Rotation as R
+from posym.operations.rotation import rotation
+from posym.operations.reflection import reflection
 from posym.tools import standardize_vector
 import numpy as np
-import hashlib
-
-
-def rotation(angle, rotation_axis):
-
-    rotation_vector = angle * np.array(rotation_axis) / np.linalg.norm(rotation_axis)
-    rotation = R.from_rotvec(rotation_vector)
-
-    return rotation.as_matrix()
 
 
 def prepare_vector(positions, vector):
     return np.array(vector) + np.array(positions)
 
 
-class Rotation(Operation):
+class ImproperRotation(Operation):
     def __init__(self, label, axis, order=1):
         super().__init__(label)
 
@@ -25,10 +17,8 @@ class Rotation(Operation):
         self._order = order
 
     def __hash__(self):
-
         axis = np.round(self._axis, decimals=6)
         axis[axis == 0.] = 0.
-
 
         return hash((self._label,
                      np.array2string(axis),
@@ -38,30 +28,25 @@ class Rotation(Operation):
     def __eq__(self, other):
         return hash(self) == hash(other)
 
-
     def get_measure(self, coordinates, modes, symbols, orientation=None):
 
         rotated_axis = self._axis if orientation is None else orientation.apply(self._axis)
 
-        #print(self._measure_mode)
-
         measure_mode = []
+        for angle in np.arange(2*np.pi/self._order, 2*np.pi, 2*np.pi/self._order)[::2]:
+            operation1 = rotation(angle, rotated_axis)
+            operation2 = reflection(rotated_axis)
+            operation = np.dot(operation2, operation1)
 
-        # print('llñ', list(np.linspace(2*np.pi/self._order, 2*np.pi, self._order)))
-        for angle in np.linspace(2*np.pi/self._order, 2*np.pi, self._order)[:-1]:
-            operation = rotation(angle, rotated_axis)
-            # operated_coor = np.dot(operation, coordinates.T).T
+            operated_coor = np.dot(operation, coordinates.T).T
 
             mesure_coor, permu = self.get_permutation(operation, coordinates, symbols)
 
             measure_mode_list = []
             for mode in modes:
 
-                # operated_mode = np.dot(operation, prepare_vector(coordinates, mode).T).T - operated_coor
-                operated_mode = np.dot(operation, np.array(mode).T).T
-
+                operated_mode = np.dot(operation, prepare_vector(coordinates, mode).T).T - operated_coor
                 norm = np.linalg.norm(mode)
-
                 permu_mode = np.array(operated_mode)[permu]
 
                 measure_mode_list.append(np.trace(np.dot(mode, permu_mode.T))/norm)
@@ -77,10 +62,13 @@ class Rotation(Operation):
         rotated_axis = self._axis if orientation is None else orientation.apply(self._axis)
 
         measure_coor = []
-        for angle in np.linspace(2*np.pi/self._order, 2*np.pi, self._order)[:-1]:
-            operation = rotation(angle, rotated_axis)
+        for angle in np.arange(2*np.pi/self._order, 2*np.pi, 2*np.pi/self._order)[::2]:
+            operation1 = rotation(angle, rotated_axis)
+            operation2 = reflection(rotated_axis)
+            operation = np.dot(operation2, operation1)
 
             mesure_coor, permu = self.get_permutation(operation, coordinates, symbols)
+
             measure_coor.append(mesure_coor)
 
         measure_coor_total = np.average(measure_coor)
@@ -98,8 +86,14 @@ class Rotation(Operation):
 
     @property
     def operation_matrix_list(self):
-        return [rotation(angle, self._axis) for angle in
-                np.linspace(2*np.pi/self._order, 2*np.pi, self._order)[:-1]]
+
+        op_matrix_list = []
+        for angle in np.linspace(2*np.pi/self._order, 2*np.pi, self._order)[:-1]:
+            operation1 = rotation(angle, self._axis)
+            operation2 = reflection(self._axis)
+            operation = np.dot(operation2, operation1)
+            op_matrix_list.append(operation)
+        return op_matrix_list
 
     def __mul__(self, other):
         if not other.__class__.__bases__[0] is Operation:
@@ -108,6 +102,6 @@ class Rotation(Operation):
             op_list = []
             for op_mat in other.operation_matrix_list:
                 new_axis = np.dot(op_mat, self._axis)
-                op_list.append(Rotation(self._label, new_axis, self._order))
+                op_list.append(ImproperRotation(self._label, new_axis, self._order))
 
             return op_list

posym/operations/reflection.py
@@ -1,76 +1,74 @@
 import numpy as np
 from posym.operations import Operation
-
-
-def prepare_vector(positions, vector):
-    return np.array(vector) + np.array(positions)
-
-
-def get_perpendicular(vector):
-    for i, v in enumerate(vector):
-        if np.abs(v) < 1e-5:
-            v1 = np.zeros_like(vector)
-            v1[i] = 1
-            v2 = np.cross(vector, v1).tolist()
-
-            return v1, v2
-
-    for i, v in enumerate(vector):
-        if np.abs(v) >= 1e-5:
-
-            vr = np.array(vector).copy()
-            vr[i] = -vr[i]
-
-            v1 = np.cross(vector, vr).tolist()
-            v2 = np.cross(vector, v1).tolist()
-
-            return v1, v2
+from posym.tools import standardize_vector
 
 
 def reflection(reflection_axis):
+    uax = np.dot(reflection_axis, reflection_axis)
 
-    reflection_axis = np.array(reflection_axis) / np.linalg.norm(reflection_axis)
-
-    axis1, axis2 = get_perpendicular(reflection_axis)
-
-    return np.outer(axis1, axis1) + np.outer(axis2, axis2) - np.outer(reflection_axis, reflection_axis)
+    return np.identity(3) - 2*np.outer(reflection_axis, reflection_axis)/uax
 
 
 class Reflection(Operation):
     def __init__(self, label, axis):
         super().__init__(label)
 
-        self._axis = axis
+        self._axis = standardize_vector(axis)
+
+    def __hash__(self):
+        axis = np.round(self._axis, decimals=6)
+        axis[axis == 0.] = 0.
+
+        return hash((self._label,
+                     np.array2string(axis)
+                     ))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
 
     def get_measure(self, coordinates, modes, symbols, orientation=None):
 
         rotated_axis = self._axis if orientation is None else orientation.apply(self._axis)
-
         operation = reflection(rotated_axis)
 
-        operated_coor = np.dot(operation, coordinates.T).T
-
         measure_mode = []
-        #measure_coor = []
-
         mesure_coor, permu = self.get_permutation(operation, coordinates, symbols)
 
-        for mode in modes:
+        for i, mode in enumerate(modes):
 
-            operated_mode = np.dot(operation, prepare_vector(coordinates, mode).T).T - operated_coor
-            #norm_1 = np.dot(np.linalg.norm(mode, axis=1), np.linalg.norm(mode, axis=1))
+            operated_mode = np.dot(operation, np.array(mode).T).T
             norm = np.linalg.norm(mode)
+            permu_mode = operated_mode[permu]
 
-            permu_mode = np.array(operated_mode)[permu]
+            measure_mode.append(np.trace(np.dot(mode, permu_mode.T))/norm)
 
-            # norm_2 = np.linalg.norm(permu_mode, axis=1)
+        return np.array(measure_mode)
 
-            #self._measure_mode.append(np.nanmean(np.divide(np.diag(np.dot(mode, permu_mode.T)), norm_1 * norm_2)))
-            measure_mode.append(np.add.reduce(np.diag(np.dot(mode, permu_mode.T)))/norm)
-            #measure_coor.append(mesure_coor)
+    def get_measure_pos(self, coordinates, symbols, orientation=None):
 
-        return np.array(measure_mode), mesure_coor
+        rotated_axis = self._axis if orientation is None else orientation.apply(self._axis)
+
+        operation = reflection(rotated_axis)
+        mesure_coor, permu = self.get_permutation(operation, coordinates, symbols)
+
+        return mesure_coor
 
     @property
     def axis(self):
         return self._axis
+
+    @property
+    def operation_matrix_list(self):
+        return [reflection(self._axis)]
+
+    def __mul__(self, other):
+        if not other.__class__.__bases__[0] is Operation:
+            raise Exception('Product only defined between Operation subclasses')
+        else:
+            op_list = []
+            for op_mat in other.operation_matrix_list:
+                new_axis = np.dot(op_mat, self._axis)
+                op_list.append(Reflection(self._label, new_axis))
+
+            return op_list