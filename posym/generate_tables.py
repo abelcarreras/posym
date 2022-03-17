89ad8c74736833851a7fb1fb0410fe8bba3adb25
Abel Carreras <abelcarreras83@gmail.com>
2022-03-17 14:08:23 +0100

    implmented group Sn

--- posym/generate_tables.py
+++ posym/generate_tables.py
@@ -208,7 +208,6 @@ def get_cnh(n):
                 element = list(ir_data_new[data])[1+i] * np.sign(list(ir_data_new[data])[(n-1)//2+1])
                 ir_data_new[data] = pd.Series(list(ir_data_new[data]) + [element])
 
-
         if n == 1:
             rotations = ["A'", "A'", "A''"]
             translations = ["A''", "A''", "A'"]
@@ -226,10 +225,117 @@ def get_cnh(n):
                      translations=translations,  # Rx, Ry, Rz
                      multiplicities=multiplicites)
 
+def get_sn(n):
+
+    if np.mod(n, 2) != 0:
+        raise Exception('n must be even')
+
+    if np.mod(n, 4) == 0:
+
+        r = np.clip(np.mod(n-1, 2), 0, 1)
+        l = (n-r-1)//2
+        ndim = 1 + l + r
+
+        multiplicity = [1] + [2] * l + [1] * r
+
+        operations = [Identity(label='E')]
+        for i in range(n//2):
+            j = gcd(i+1, n)
+            up = (i+1)//j
+            down = n//j
+            if np.mod(i, 2) == 0:
+                label = 'S_{}'.format(down) if up == 1 else 'S^{}_{}'.format(up, down)
+                operations.append(ImproperRotation(label=label, axis=[0, 0, 1], order=down, exp=up))
+            else:
+                label = 'C_{}'.format(down) if up == 1 else 'C^{}_{}'.format(up, down)
+                operations.append(Rotation(label=label, axis=[0, 0, 1], order=down, exp=up))
+
+        ir_data = {'A': pd.Series([1] * ndim)}
+        if r == 1:
+            ir_data.update({'B': pd.Series([(-1)**k for k in range(ndim)])})
+
+        for i in range(l):
+            label = 'E{}'.format(i+1) if l > 1 else 'E'
+            ir_data.update({label: pd.Series([2] + [real_radical((i+1)*(k+1), n) for k in range(ndim-1)])})
+
+        if n == 4:
+            rotations = ['E', 'E', 'A']
+            translations = ['E', 'E', 'B']
+        else:
+            rotations = ['E1', 'E1', 'A']
+            translations = ['E{}'.format(l//2), 'E{}'.format(l//2), 'B']
+
+    else:
+
+        r = np.clip(np.mod(n-1, 2), 0, 1)
+        l = (n-r-1)//2
+        ndim = 1 + l + r
+
+        multiplicity = [1] + [2] * l + [1] * r
+
+        operations = [Identity(label='E')]
+        for i in range(n//2):
+            j = gcd(i+1, n)
+            up = (i+1)//j
+            down = n//j
+            if np.mod(i, 2) == 0:
+                if up == 1 and down == 2:
+                    label = 'i'.format(down)
+                    operations.append(Inversion(label=label))
+                else:
+                    label = 'S_{}'.format(down) if up == 1 else 'S^{}_{}'.format(up, down)
+                    operations.append(ImproperRotation(label=label, axis=[0, 0, 1], order=down, exp=up))
+            else:
+                label = 'C_{}'.format(down) if up == 1 else 'C^{}_{}'.format(up, down)
+                operations.append(Rotation(label=label, axis=[0, 0, 1], order=down, exp=up))
+
+        ir_data = {'Ag': pd.Series([1] * ndim)}
+        traces = [1]
+        for i in range(n//2):
+            if np.mod(i, 2) == 0:
+                traces.append(-1)
+            else:
+                traces.append(1)
+
+        ir_data.update({'Au': pd.Series(traces)})
+
+        for i in range(0, l//2):
+            subtrace = np.roll([k+1 for k in range(n//4)], -i)
+            label = 'E{}g'.format(i+1) if l > 2 else 'Eg'
+            ir_data.update({label: pd.Series([2] + [real_radical(k, n)*(-1)**(k+2*i) for k in subtrace] + [real_radical(k, n)*(-1)**(k+2*i) for k in subtrace[::-1]] + [2])})
+            label = 'E{}u'.format(i+1) if l > 2 else 'Eu'
+            ir_data.update({label: pd.Series([2] + [real_radical(k, n)*(-1)**(k+2*i + k//2) for k in subtrace] + [real_radical(k, n)*(-1)**(k+2*i + k//2 + 1) for k in subtrace[::-1]] + [-2])})
+
+        if n == 2:
+            rotations = ['Ag', 'Ag', 'Ag']
+            translations = ['Au', 'Au', 'Au']
+        elif n == 6:
+            rotations = ['Eg', 'Eg', 'Ag']
+            translations = ['Eu', 'Eu', 'Au']
+        else:
+            rotations = ['E1g', 'E1g', 'Ag']
+            translations = ['E1u', 'E1u', 'Au']
+
+    return CharTable('S{}'.format(n),
+                     operations,
+                     ir_data,
+                     rotations=rotations,  # x, y, z
+                     translations=translations,  # Rx, Ry, Rz
+                     multiplicities=multiplicity)
+
+
 if __name__ == '__main__':
 
-    for i in range(1, 9):
-        print(get_cnh(i))
+    #for i in range(1, 9):
+    #    print(get_cnh(i))
+
+    print(get_sn(2))
+    print(get_sn(4))
+    print(get_sn(6))
+    print(get_sn(8))
+    print(get_sn(10))
+    print(get_sn(12))
+    print(get_sn(14))
 
     exit()
     get_cn(2)
