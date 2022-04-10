fix direct product and move normalization to symmetry subclasses

posym/__init__.py
@@ -25,7 +25,7 @@ class SymmetryBase():
         if isinstance(rep, str):
             if rep not in self._pg.ir_labels:
                 raise Exception('Representation do not match with group\nAvailable: {}'.format(self._pg.ir_labels))
-            self._op_representation = self._pg.ir_table[rep]/self._pg.ir_table[rep]['E']
+            self._op_representation = self._pg.ir_table[rep]
 
         elif isinstance(rep, pd.Series):
             if np.all(self._pg.ir_table.sort_index().index == rep.sort_index().index):
@@ -138,11 +138,15 @@ class SymmetryModes(SymmetryBase):
             mode_measures = np.average(mode_measures, axis=0)
             self._mode_measures.append(mode_measures)
 
+        # Normalization
+        self._mode_measures = np.dot(pg.trans_matrix, np.dot(pg.trans_matrix_inv_norm, self._mode_measures))
+
         total_state = pd.Series(np.add.reduce(self._mode_measures, axis=1), index=pg.op_labels)
 
         super().__init__(group, total_state)
 
     def get_state_mode(self, n):
+
         return SymmetryBase(group=self._group, rep=pd.Series(np.array(self._mode_measures).T[n],
                                                              index=self._pg.op_labels))
 
@@ -235,6 +239,9 @@ class SymmetryFunction(SymmetryBase):
 
             operator_measures = np.average(operator_measures, axis=0)
             self._operator_measures.append(operator_measures)
+
+        # Normalization
+        self._operator_measures = np.dot(pg.trans_matrix, np.dot(pg.trans_matrix_inv_norm, self._operator_measures))
 
         total_state = pd.Series(self._operator_measures, index=pg.op_labels)
 

posym/pointgroup.py
@@ -12,6 +12,8 @@ class PointGroup():
 
         self._trans_matrix = None
         self._trans_matrix_inv = None
+        self._trans_matrix_norm = None
+        self._trans_matrix_inv_norm = None
 
         # get IR table
         self._table = get_table_from_label(group)
@@ -67,9 +69,24 @@ class PointGroup():
         """
         if self._trans_matrix is None:
             self._trans_matrix = np.array([v for v in self.ir_table.T.values]).T
-            self._trans_matrix = self._trans_matrix/self._trans_matrix[0, :]  # normalization
+            self._trans_matrix = self._trans_matrix
 
         return self._trans_matrix
+
+    @property
+    def trans_matrix_norm(self):
+        """
+        transforms IR to Op
+
+        Op  = Mat * IR
+
+        :return: the transformation matrix
+        """
+        if self._trans_matrix_norm is None:
+            self._trans_matrix_norm = np.array([v for v in self.ir_table.T.values]).T
+            self._trans_matrix_norm = self._trans_matrix_norm/self._trans_matrix_norm[0, :]  # normalization
+
+        return self._trans_matrix_norm
 
     @property
     def trans_matrix_inv(self):
@@ -85,6 +102,21 @@ class PointGroup():
             self._trans_matrix_inv = np.linalg.inv(self.trans_matrix)
 
         return self._trans_matrix_inv
+
+    @property
+    def trans_matrix_inv_norm(self):
+        """
+        transforms Op to IR
+
+        IR = Mat * Op
+
+        :return: the transformation matrix
+        """
+
+        if self._trans_matrix_inv_norm is None:
+            self._trans_matrix_inv_norm = np.linalg.inv(self.trans_matrix_norm)
+
+        return self._trans_matrix_inv_norm
 
     @property
     def order(self):