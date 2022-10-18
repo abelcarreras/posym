9c02da82f9b0fd223de2c5ef3c80e19fdfbdb1bc
Abel Carreras <abelcarreras83@gmail.com>
2022-10-18 16:23:21 +0200

    update tests

--- test/modes_test.py
+++ test/modes_test.py
@@ -6,52 +6,61 @@ import os
 import json
 
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 class ModesTest(unittest.TestCase):
+    longMessage = True
 
-    def test_from_files(self):
-        for j, filename in enumerate(os.listdir()):
-            with self.subTest(filename, i=j):
 
-                if filename.endswith('json'):
-                    print(filename)
-                    group = filename.split('_')[0]
+def make_test_function(filename, group):
+    def test(self):
+        with open(os.path.join(dir_path, filename), 'r') as openfile:
+            # Reading from json file
+            json_object = json.load(openfile)
 
-                    with open(filename, 'r') as openfile:
-                        # Reading from json file
-                        json_object = json.load(openfile)
+        molecule_coor = np.array(json_object['coordinates'])
+        molecule_symbols = np.array(json_object['symbols'])
+        modes = [np.array(m['displacement']) for m in json_object['modes']]
+        freqs = [m['frequency'] for m in json_object['modes']]
 
-                    molecule_coor = np.array(json_object['coordinates'])
-                    molecule_symbols = np.array(json_object['symbols'])
-                    modes = [np.array(m['displacement']) for m in json_object['modes']]
-                    freqs = [m['frequency'] for m in json_object['modes']]
+        sm = SymmetryModes(group=group,
+                           coordinates=molecule_coor,
+                           modes=modes,
+                           symbols=molecule_symbols,
+                           )
 
-                    sm = SymmetryModes(group=group,
-                                       coordinates=molecule_coor,
-                                       modes=modes,
-                                       symbols=molecule_symbols,
-                                       )
+        def localization(measure_list):
+            import itertools
+            prod_list = []
+            for a in itertools.permutations(measure_list, 2):
+                prod_list.append(np.prod(a))
+            return np.sum(prod_list)
 
-                    def localization(measure_list):
-                        import itertools
-                        prod_list = []
-                        for a in itertools.permutations(measure_list, 2):
-                            prod_list.append(np.prod(a))
-                        return np.sum(prod_list)
+        pos_measure = sm.get_measure_pos
+        print('fun: ', pos_measure)
+        total_loc = []
+        for i in range(len(modes)):
+            print('m {:2}: {:8.3f} :'.format(i + 1, freqs[i]), sm.get_state_mode(i))
+            total_loc.append(localization(sm.get_state_mode(i).get_ir_representation().values))
 
-                    pos_measure = sm.get_measure_pos
-                    print('fun: ', pos_measure)
-                    total_loc = []
-                    for i in range(len(modes)):
-                        print('m {:2}: {:8.3f} :'.format(i + 1, freqs[i]), sm.get_state_mode(i))
-                        total_loc.append(localization(sm.get_state_mode(i).get_ir_representation().values))
+        max_loc = np.max(np.abs(total_loc))
 
-                    max_loc = np.max(np.abs(total_loc))
+        print('Total: ', sm)
+        print('Norm: ', al.norm(sm), len(molecule_symbols) * 3 - 6)
+        print('Dot: ', al.dot(sm, sm))
+        print('angles: ', sm.orientation_angles)
+        print('loc: ', max_loc)
 
-                    print('Total: ', sm)
-                    print('Norm: ', al.norm(sm), len(molecule_symbols) * 3 - 6)
-                    print('Dot: ', al.dot(sm, sm))
-                    print('angles: ', sm.orientation_angles)
-                    print('loc: ', max_loc)
+        self.assertLess(max_loc, 1e-2)
+        self.assertLess(pos_measure, 1e-2)
 
-                    self.assertLess(max_loc, 1e-2)
-                    self.assertLess(pos_measure, 1e-2)
+    return test
+
+
+for j, filename in enumerate(os.listdir()):
+
+    if filename.endswith('json'):
+        group = filename.split('_')[0]
+        test_func = make_test_function(filename, group)
+        setattr(ModesTest, 'test_{0}'.format(filename[:-5]), test_func)
+        del test_func
