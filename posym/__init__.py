Fix issue in molecular geometry symmetry calculation when setting the molecule in the geometric center

posym/__init__.py
@@ -131,15 +131,17 @@ class SymmetryMoleculeBase(SymmetryBase):
         self._setup_structure(coordinates, symbols, group, center, orientation_angles)
 
         if total_state is None:
-            rotmol = R.from_euler('zyx', self._angles, degrees=True).inv()
+            rotmol = R.from_euler('zyx', self._angles, degrees=True)
+            geom_center = np.average(coordinates, axis=0)
+            centered_coor = np.subtract(coordinates, geom_center)
+
             self._operator_measures = []
             for operation in self._pg.operations:
                 operations_dic = self._pg.get_all_operations()
                 operator_measures = []
                 for op in operations_dic[operation.label]:
-                    overlap = op.get_measure_pos(np.array(coordinates), symbols, orientation=rotmol)
-                    geom_center = np.average(coordinates, axis=0)
-                    measure_norm = np.average(np.linalg.norm(np.subtract(coordinates, geom_center), axis=1))
+                    overlap = op.get_measure_pos(centered_coor, symbols, orientation=rotmol)
+                    measure_norm = np.average(np.linalg.norm(centered_coor, axis=1))
                     operator_measures.append(1-overlap/measure_norm)
 
                 self._operator_measures.append(np.array(operator_measures))

posym/ir_tables.py
@@ -311,6 +311,6 @@ if __name__ == '__main__':
     print(0, 1, 0)
     exit()
 
-    #[Identity(label='E'), Rotation(label='C3', axis=[0, 0, 1], order=3),
+    # [Identity(label='E'), Rotation(label='C3', axis=[0, 0, 1], order=3),
     # Rotation(label='C2', axis=[np.sqrt(8 / 9), 0, -1 / 3], order=2),
     # ImproperRotation(label='S4', axis=[np.sqrt(8 / 9), 0, -1 / 3], order=4),

posym/tools.py
@@ -1,12 +1,11 @@
-from itertools import combinations
 from copy import deepcopy
 import numpy as np
 from posym.basis import BasisFunction, PrimitiveGaussian
 
 
-def list_round(list, decimals=2):
+def list_round(elements_list, decimals=2):
     r_list = []
-    for element in list:
+    for element in elements_list:
         if abs(np.round(element) - element) < 10**(-decimals):
             r_list.append(int(np.round(element)))
         else: