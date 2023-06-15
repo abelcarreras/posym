31dd8e17ae18d4e7984d56ad53746a4154340e3b
Abel Carreras <abelcarreras83@gmail.com>
2023-06-15 16:25:53 +0200

    clean and improve performance of permutation functions

--- posym/operations/__init__.py
+++ posym/operations/__init__.py
@@ -1,63 +1,15 @@
 import numpy as np
-from posym.permutations import get_cross_distance_table, get_permutation_simple  # noqa
+from functools import lru_cache
 
 
-def get_cross_distance_table_py(coordinates_1, coordinates_2):
+@lru_cache(maxsize=100)
+def get_submatrix_indices(symbols):
+    # separate distance_table in submatrices corresponding to a single symbol
+    submatrices_indices = []
+    for s in np.unique(symbols):
+        submatrices_indices.append([j for j, s2 in enumerate(symbols) if s2 == s])
 
-    coordinates_1 = np.array(coordinates_1)
-    coordinates_2 = np.array(coordinates_2)
-
-    distances = np.zeros((len(coordinates_1), len(coordinates_2)))
-
-    for i, c1 in enumerate(coordinates_1):
-        for j, c2 in enumerate(coordinates_2):
-            # print(i, j, c1, c2, np.linalg.norm(c1 - c2))
-            distances[i, j] = np.linalg.norm(c1 - c2)
-    return distances
-
-
-def get_permutation_simple_py(distance_table, symbols):
-
-    def evaluation(i, j, row1, row2):
-        from scipy.stats import norm
-
-        s1 = np.sum([val**2 * norm.pdf(k, loc=i, scale=0.1) for k, val in enumerate(row1)])
-        s2 = np.sum([val**2 * norm.pdf(k, loc=j, scale=0.1) for k, val in enumerate(row2)])
-
-        return s1 + s2
-
-    def evaluation_fast(i, j, row1, row2):
-        off_diagonal = (row1[j] - row2[i]) ** 2
-        return row1[i] ** 2 + row2[j] ** 2 + off_diagonal
-
-    perm = np.array(range(len(distance_table)))
-    distance_table = np.array(distance_table).copy()
-
-    control = True
-    while control:
-        control = False
-        for a in range(len(distance_table)-1, 1, -1):
-            for i in range(len(distance_table)):
-                j = i+a if i+a < len(distance_table) else i+a - len(distance_table)
-
-                row_1 = distance_table[i]
-                row_2 = distance_table[j]
-
-                if symbols[i] == symbols[j]:
-                    if evaluation_fast(i, j, row_1, row_2) > evaluation_fast(j, i, row_1, row_2):
-                        distance_table[[i, j]] = distance_table[[j, i]]
-                        perm[[i, j]] = perm[[j, i]]
-                        control = True
-
-                # print(i, j, evaluation_fast(i, j, row_1, row_2), evaluation_fast(j, i, row_1, row_2))
-                # print(distance_table)
-
-    def inverse_perm(perm):
-        return [list(perm).index(j) for j in range(len(perm))]
-
-    # print('-->\n', np.round(distance_table, decimals=3))
-    # print(np.round(np.diag(distance_table), decimals=3))
-    return inverse_perm(perm)
+    return submatrices_indices
 
 
 def get_permutation_hungarian(distance_table, symbols):
@@ -73,12 +25,7 @@ def get_permutation_hungarian(distance_table, symbols):
         perm[row_ind] = col_ind
         return perm
 
-    # return get_perm_submatrix(distance_table)
-
-    # separate distance_table in submatrices corresponding to a single symbol
-    submatrices_indices = []
-    for s in np.unique(symbols):
-        submatrices_indices.append([j for j, s2 in enumerate(symbols) if s2 == s])
+    submatrices_indices = get_submatrix_indices(symbols)
 
     # determine the permutation for each submatrix
     perm_submatrices = []
@@ -93,7 +40,7 @@ def get_permutation_hungarian(distance_table, symbols):
         index = np.array(index)
         global_permutation[index] = index[perm]
 
-    return global_permutation
+    return list(global_permutation)
 
 
 class Operation:
@@ -101,20 +48,20 @@ class Operation:
         self._label = label
 
     def get_permutation(self, operation, coordinates, symbols, return_dot=False):
-        operated_coor = np.dot(operation, coordinates.T).T
 
-        symbols = [int.from_bytes(num.encode(), 'big') for num in symbols]
-        distance_table = get_cross_distance_table(coordinates, operated_coor)
-        # perm = get_permutation_simple(distance_table, symbols)
-        perm = get_permutation_hungarian(distance_table, symbols)
-        permu_coor = operated_coor[list(perm)]
+        operated_coor = np.dot(operation, coordinates.T).T
+        symbols = tuple(int.from_bytes(num.encode(), 'big') for num in symbols)
+
+        dot_table = -np.dot(coordinates, operated_coor.T)
+        permutation = get_permutation_hungarian(dot_table, symbols)
 
         if return_dot:
+            permu_coor = operated_coor[permutation]
             measure = np.einsum('ij, ij -> ', coordinates, permu_coor)
             # measure = np.trace(np.dot(coordinates, permu_coor.T))
-            return measure, list(perm)
+            return measure, permutation
         else:
-            return list(perm)
+            return permutation
 
     def get_normalization(self, coordinates):
 
