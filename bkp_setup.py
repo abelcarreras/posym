caa1d6ee1f19acae4756e566f84294bc30a239be
jaclark5 <jennifer.clark@openforcefield.org>
2025-10-14 13:58:24 -0400

    Update installation with toml

--- bkp_setup.py
+++ bkp_setup.py
@@ -3,13 +3,6 @@ import numpy
 
 include_dirs_numpy = [numpy.get_include()]
 
-def get_version_number():
-    main_ns = {}
-    for line in open('posym/__init__.py', 'r').readlines():
-        if not(line.find('__version__')):
-            exec(line, main_ns)
-            return main_ns['__version__']
-
 def check_compiler():
     import subprocess
     output = subprocess.Popen(['gcc'], stderr=subprocess.PIPE).communicate()[1]
@@ -39,17 +32,4 @@ permutations = Extension('posym.permutation.permutations',
                          sources=['c/permutations.c'])
 
 
-setup(name='posym',
-      version=get_version_number(),
-      description='posym module',
-      long_description=open('README.md').read(),
-      long_description_content_type='text/markdown',
-      author='Abel Carreras',
-      url='https://github.com/abelcarreras/posym',
-      author_email='abelcarreras83@gmail.com',
-      packages=['posym',
-                'posym.operations',
-                'posym.permutation'],
-      install_requires=['numpy', 'scipy', 'pandas', 'PyYaml'],
-      license='MIT License',
-      ext_modules=[integrals, permutations])
+setup(ext_modules=[integrals, permutations])
--- posym/__init__.py
+++ posym/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Abel Carreras'
-__version__ = '1.2.4'
+from ._version import __version__ as __version__
 
 from posym.tools import list_round, get_principal_axis_angles
 from posym.pointgroup import PointGroup
